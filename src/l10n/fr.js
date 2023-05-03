e6cf55f56ef20ef751f98c89d3d9e4466074da94
JF <jfcartier@kiwad.com>
2023-05-03 14:19:49 -0400

    French translation added

---
+++ src/l10n/fr.js
@@ -0,0 +1,7 @@
+export default {
+    lightboxLabel: 'Ceci est une fenêtre de dialogue qui se superpose au contenu principal de la page. Elle affiche l’image agrandie. En appuyant sur la touche Echap, vous fermerez la fenêtre et reviendrez à l’endroit où vous étiez sur la page.',
+    lightboxLoadingIndicatorLabel: 'Chargement de l’image',
+    previousButtonLabel: 'Image précédente',
+    nextButtonLabel: 'Image suivante',
+    closeButtonLabel: 'Fermer la fenêtre de dialogue'
+  }
